02e50d796a8bd23e3daea73fa07b831c4b859563
Jmrxr <jairenriquez30@gmail.com>
2025-01-30 18:27:41 -0500

    add

--- test.py
+++
@@ -1,8 +0,0 @@
-# Programa que determine si un número es par o impar.
-
-number = int(input("Enter a number:"))
-
-if number % 2 == 0:
-    print("The number is even.")
-else:
-    print("The number is odd.")
